4154648741962d2f38d79adfe2030cda71a3ac95
worknech <worknech@yandex.ru>
2025-10-16 22:31:13 +0300

    Добавление фикстур для goods

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
