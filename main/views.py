24d4c0b3856f5e19331591e94e10b91c01a10758
worknech <worknech@yandex.ru>
2025-10-18 16:40:25 +0300

    Регистрация simple_tag для категорий

--- main/views.py
+++ main/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
+    
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
-        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
