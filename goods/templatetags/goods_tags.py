c890357d32937947dee1aea1777174d9705bf94b
worknech <worknech@yandex.ru>
2025-10-20 19:18:23 +0300

    Фильтры на странице сайта и urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.http import urlencode
+
 from goods.models import Categories
 
 
@@ -25,3 +27,10 @@ def tag_categories():
         - Для использования в шаблоне требуется предварительная загрузка тега через `{% load %}`.
     """
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
